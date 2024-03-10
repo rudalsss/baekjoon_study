a86c91301c2366bc3236b59b9a7024df16e73054
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-03-10 15:41:36 +0900

    [Bronze V] Title: A-B, Time: 204 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        int a,b;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+        int c = a-b;
+        System.out.println(c);
+    }
+}
