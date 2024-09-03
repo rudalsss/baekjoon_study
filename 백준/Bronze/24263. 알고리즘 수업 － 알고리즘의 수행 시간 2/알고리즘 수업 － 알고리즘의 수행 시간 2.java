ff8d6b57a24fc499a2c1a60722e0fc036cf44a50
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-09-03 19:59:36 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 164 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        System.out.println(n);  // 반복문 수행횟수
+        System.out.println(1);  // 시간복잡도 O(1)
+    }
+}
