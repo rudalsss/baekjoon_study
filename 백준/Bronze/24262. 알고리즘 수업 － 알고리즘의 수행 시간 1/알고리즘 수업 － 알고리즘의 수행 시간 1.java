9ec6c7add0f300487de8cae28a73bf7911c6958b
Kyungmin <122061889+rudalsss@users.noreply.github.com>
2024-09-03 17:12:45 +0900

    Update 알고리즘 수업 － 알고리즘의 수행 시간 1.java

--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 입력 값
         
-        System.out.println(1);
-        System.out.println(0);
+        System.out.println(1);    // 코드의 실행횟수(고정)
+        System.out.println(0);    // 시간복잡도 O(1)
         
+        sc.close();
     }
 }
