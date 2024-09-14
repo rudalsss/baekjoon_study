6913db366325f77d2900a2e1608156f40fdc85bf
Kyungmin Lee <122061889+rudalsss@users.noreply.github.com>
2024-09-14 18:39:59 +0900

    [Silver V] Title: 영화감독 숌, Time: 320 ms, Memory: 88512 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int cnt = 1;
+    int num = 666;
+
+    while(cnt<n){
+      num++;
+      if(String.valueOf(num).contains("666")) cnt++;
+    }
+
+    System.out.println(num);
+  }
+}
