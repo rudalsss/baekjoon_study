3f6f9bebbde1c2f315a20fe0953f38a6a6bd0210
Kyungmin Lee <122061889+rudalsss@users.noreply.github.com>
2024-09-21 15:54:13 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1712 ms, Memory: 72100 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int n = Integer.parseInt(br.readLine());
+
+            int[] x_values = new int[n];
+            int[] y_values = new int[n];
+            Integer[] index_values = new Integer[n]; //좌표순서
+
+            for(int i=0; i<n; i++){
+                String line = br.readLine(); //한줄 읽기
+                String[] nums = line.split(" "); //공백으로 분리
+                x_values[i] = Integer.parseInt(nums[0]);
+                y_values[i] = Integer.parseInt(nums[1]);
+                index_values[i] = i;
+            }
+
+            /* 버블정렬 이용
+            int a = n;
+            while(a>0){
+                for(int i=0; i<a-1; i++){
+                    if(x_values[i] > x_values[i+1]){
+                        // x좌표 정렬
+                        int temp = x_values[i+1];
+                        x_values[i+1] = x_values[i];
+                        x_values[i] = temp;
+
+                        // y좌표 정렬
+                        temp = y_values[i+1];
+                        y_values[i+1] = y_values[i];
+                        y_values[i] = temp;
+                    } else if(x_values[i] == x_values[i+1]){
+                        if(y_values[i] >y_values[i+1]){
+                            int temp = y_values[i+1];
+                            y_values[i+1] = y_values[i];
+                            y_values[i] = temp;
+
+                            temp = x_values[i+1];
+                            x_values[i+1] = x_values[i];
+                            x_values[i] = temp;
+                        }
+                    }
+                }
+                a--;
+            }
+             */
+
+            // 인덱스 정보 배열을 정렬
+            Arrays.sort( index_values, (a,b) -> {
+                // x값이 같지 않으면 x값으로 오름차순 정렬
+                if ( x_values[a] !=  x_values[b] ) return Integer.compare(x_values[a], x_values[b]);
+                // x값이 같다면 y값으로 오름차순 정렬
+                return Integer.compare(y_values[a], y_values[b]);
+            });
+
+            //출력
+            for(int i=0; i<n; i++){
+                int index = index_values[i];
+                System.out.print(x_values[index]+" "+y_values[index]);
+                System.out.println();
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
